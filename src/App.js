7524e189a465dc3135db9a1d75ae46d1b3cc1b85
Lucas <lucas@Ashleys-MacBook-Pro.local>
2020-02-27 10:24:48 -0700

    Morning Review #2 - Finished

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import './App.css'
+import List from './Components/List'
 
+function App() {
+    return(
+        <div className='App'>
+            <h1 className='movie-list-title'>
+                The Most Amazing Movie List
+            </h1>
+            <List/>
+        </div>
+    )
+}
+
+export default App
